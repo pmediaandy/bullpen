5ebaceffbf198b1a74ff7e0da186b171f90195b4
Chris Huang <chenhsiu@gmail.com>
2016-04-16 16:25:50 +0800

    add minmaxdiv

---
+++ codility/minmaxdiv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def show(S):
+    print S
+    pass
+
+def solution(K, M, A):
+    show(A)
+    N = len(A)
+
+if __name__ == '__main__':
+    print solution(3, 5, [2, 1, 5, 1, 2, 2, 2]) == 6
+
